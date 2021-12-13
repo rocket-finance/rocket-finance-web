7d82e3f1f65bfc7d55b09d15ae75170673ac0ab9
tapswap <tapswapmoneydex@gmail.com>
2021-12-13 17:08:45 +0800

    chore: unlock button color

--- src/components/UnlockButton.tsx
+++ src/components/UnlockButton.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { Button, useWalletModal } from 'rocket-finance-uikit'
 import useAuth from 'hooks/useAuth'
 import useI18n from 'hooks/useI18n'
+import styled from 'styled-components'
+
+const Unlock = styled(Button)`
+  background-color: ${({ theme }) => theme.colors.textSubtle};
+  color: ${({ theme }) => theme.colors.input};
+`
 
 const UnlockButton = (props) => {
   const TranslateString = useI18n()
@@ -9,9 +15,9 @@ const UnlockButton = (props) => {
   const { onPresentConnectModal } = useWalletModal(login, logout)
 
   return (
-    <Button onClick={onPresentConnectModal} {...props}>
+    <Unlock onClick={onPresentConnectModal} {...props}>
       {TranslateString(292, 'Unlock Wallet')}
-    </Button>
+    </Unlock>
   )
 }
 
