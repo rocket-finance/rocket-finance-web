d295db6c212d590534981301ac54d237000ee8b9
tapswap <tapswapmoneydex@gmail.com>
2021-12-13 16:38:12 +0800

    chore: black and white theme

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -127,7 +127,7 @@ const Home: React.FC = () => {
         <div>
           <Cards>
             <FarmStakingCard />
-            <TwitterCard />
+            {/* <TwitterCard /> */}
             <CakeStats />
             <TotalValueLockedCard />
           </Cards>
